b857ba4dd7b38ee36153f04c1ce931a7eb538d9e
wendycan <wendycaner@gmail.com>
2017-06-17 19:08:46 +0800

    Initial Commit

---
+++ Marmoset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marmoset
+//
+//  Created by 王欣 on 6/17/17.
+//  Copyright © 2017 王欣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MarmosetTests/MarmosetTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarmosetTests.swift
+//  MarmosetTests
+//
+//  Created by 王欣 on 6/17/17.
+//  Copyright © 2017 王欣. All rights reserved.
+//
+
+import XCTest
+@testable import Marmoset
+
+class MarmosetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
